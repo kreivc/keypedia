40f42ffa5e81b6b09c6586f5b18679ab17de3204
brianjapar <brianguesse8@gmail.com>
2021-12-03 17:22:26 +0700

    styled background form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header bg-danger">{{ __('Login') }}</div>
+                <div class="card-header bg-headerform">{{ __('Login') }}</div>
 
-                <div class="card-body bg-danger">
+                <div class="card-body bg-bodyform">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -53,7 +54,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn bg-btnform">
                                     {{ __('Login') }}
                                 </button>
                             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header bg-danger">{{ __('Register') }}</div>
+                <div class="card-header bg-headerform">{{ __('Register') }}</div>
 
-                <div class="card-body bg-danger">
+                <div class="card-body bg-bodyform">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
@@ -99,7 +100,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn bg-btnform">
                                     {{ __('Register') }}
                                 </button>
                             </div>
