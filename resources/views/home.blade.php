4231968e7f51e8bb67a386644314aed805e2103f
Ricky <phangr28@gmail.com>
2021-12-10 23:34:03 +0700

    remove auth param on home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,29 +5,27 @@
     $keyboard = ['87 Key Keyboard', '61 Key Keyboard', 'XDA Profile', 'Cherry Profile'];
     @endphp
 
-    @if (Auth::check())
-        <div class="container">
-            <div class="text-center">
-                <p class="h1 font-weight-bold">Welcome to Keypedia</p>
-                <p class="h5 font-weight-bold">Best Keyboard and Keycaps Shop</p>
-                <p class="h2 font-weight-bold mt-4">Categories</p>
+    <div class="container">
+        <div class="text-center">
+            <p class="h1 font-weight-bold">Welcome to Keypedia</p>
+            <p class="h5 font-weight-bold">Best Keyboard and Keycaps Shop</p>
+            <p class="h2 font-weight-bold mt-4">Categories</p>
 
-            </div>
-
-            <div class="d-flex justify-content-center align-items-center flex-wrap">
-                @foreach ($keyboard as $key)
-                    <div class="p-1 bg-outside m-3">
-                        <div class="card bg-inside" style="width: 18rem;">
-                            <div class="card-body text-center">
-                                <a class="card-text h5" href="#">{{ $key }}</a>
-                            </div>
-                            <img src="/storage/assets/{{ $loop->index + 1 }}.jpg" class="d-block w-100"
-                                alt="{{ $loop->index + 1 }}">
-                        </div>
-                    </div>
-                @endforeach
-            </div>
         </div>
-    @endif
+
+        <div class="d-flex justify-content-center align-items-center flex-wrap">
+            @foreach ($keyboard as $key)
+                <div class="p-1 bg-outside m-3">
+                    <div class="card bg-inside" style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <a class="card-text h5" href="#">{{ $key }}</a>
+                        </div>
+                        <img src="/storage/assets/{{ $loop->index + 1 }}.jpg" class="d-block w-100"
+                            alt="{{ $loop->index + 1 }}">
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 @endsection
