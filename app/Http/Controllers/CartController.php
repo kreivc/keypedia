feb7d35c959f33dad72aeb535ac76e8609678776
brianjapar <brianguesse8@gmail.com>
2021-12-14 20:46:26 +0700

    cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
-    //
+    public function addToCart($id, Request $request){
+        $cart = Cart::where('user_id',auth()->user()->id)->where('keyboard_id',$id)->first();
+        if(!auth()){
+            return redirect()->back()->withErrors('Please login first!')->withInput();
+        }
+
+        $data = $request->all();
+        $validator = Validator::make($data,[
+            'quantity' => 'required|integer',
+        ]);
+        if($validator->fails()){
+            return redirect(route('createKeyboard'))->withErrors($validator)->withInput();
+        }
+
+ 
+        if($cart==NULL){
+            $cart=Cart::create([
+                'user_id' => auth()->user()->id,
+                'keyboard_id' => $id,
+                'quantity'=>  $request->quantity,
+            ]);
+        }else{
+            $cart->quantity = $cart->quantity+ $request->quantity;
+        }
+        $cart->save();
+        return view('userCart',compact('cart'));
+    }
 }
