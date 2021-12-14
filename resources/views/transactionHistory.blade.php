3458ad4106a7dbcef2acff483d986bab4db9e66a
Ricky <phangr28@gmail.com>
2021-12-15 01:02:57 +0700

    history fix

--- resources/views/transactionHistory.blade.php
+++ resources/views/transactionHistory.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-@foreach ( $histories as $history )
-    <a href="/transactionDetail/{{ $history->id }}" class="text-white">Transaction at {{ $history->transactionDate }}</a>
-@endforeach
+    <h2 class="display-4 text-center mb-5">Your Transaction</h2>
+    <div class="d-flex flex-column">
+        @foreach ($histories as $history)
+            <div class="justify-content-center mb-4 text-center">
+                <a href="/transactionDetail/{{ $history->id }}" class="h5 bg-white rounded-xl p-1 px-5">Transaction at
+                    {{ $history->transactionDate }}</a>
+            </div>
+        @endforeach
+    </div>
 
 
 @endsection
